965e697e26172ae2d20101a2283cdd000efbf52d
andreaainara <andrea.ainara10@gmail.com>
2016-05-30 00:13:13 -0700

    user index showing, adding crud to users and respective views... also sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  private
+
+    def set_user
+      User.find(params[:id])
+    end
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :phone, :password)
+    end
 end
