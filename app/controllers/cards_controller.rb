9ddde6c5ec2e3b35e181cc0e39019ac9867b0f7a
andreaainara <andrea.ainara10@gmail.com>
2016-06-02 09:02:58 -0700

    cards crud methods complete, but need updating as necessary

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,11 +13,43 @@ end
 # I want to be the cards#create form to be in a modal,
 # so as to limit the number of pages...
   def new
+    @card = Card.new
     render :new
   end
 
   def create
+    card_params = params.require(:card).permit(:picture, :text)
+    @board = Board.find(params[:board_id])
+    @card = Card.new(card_params)
+    @card.board = @board
+    if @card.save
 
+      redirect_to board_path(@board)
+    else
+      render 'boards/show'
+    end
+  end
+
+  def edit
+    @card = Card.find_by_id(params[:card_id])
+  end
+
+  def update
+    @board = Board.find(params[:board_id])
+    @card = Card.find(params[:card_id])
+    if @card.update(card_params)
+      @card.board = @board
+      redirect_to board_path(@board)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @board = Bard.find(params[:board_id])
+    @card = Card.find(params[:card_id])
+    @card.destroy
+    redirect_to board_path(@board)
   end
 
   private
@@ -27,8 +59,14 @@ end
   end
 
   def set_card
+    Card.find(params[:card_id])
   end
 
   def card_params
+    params.require(:card).permit(:picture, :text)
+  end
+
+  def board_params
+    params.require(:board).permit(:name)
   end
 end
